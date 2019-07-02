e584019e5f701a42a8cbc58a9b9e26f4746b90a8
luojunbo <1149216056@qq.com>
2019-07-02 19:31:53 +0800

    整理下代码

---
+++ Assets/Scripts/BVHAccelerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BVHAccelerator : MonoBehaviour
+{
+
+	private BVH bvh;
+
+	// Use this for initialization
+	void Start()
+	{
+		bvh = new BVH(null);
+		bvh.CreateBVH();
+	}
+
+	// Update is called once per fram
+
+	void OnDrawGizmos()
+	{
+		if (bvh != null)
+		{
+			bvh.DrawBVH();
+		}
+	}
+}
